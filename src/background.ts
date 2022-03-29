4cf5238ac8d7b5a9ced447a4e161961a01976b4d
Snoweuph <domsaeume@gmail.com>
2022-03-29 21:42:20 +0200

    saving window position, size and other options

--- src/background.ts
+++ src/background.ts
@@ -4,8 +4,12 @@ import { app, protocol, BrowserWindow, screen} from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 import Options from './types/options'
+import fs from 'fs';
+import {join as pathjoin} from 'path';
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+const DEFAULT_WINDOW_SIZE_X = 1000;
+const DEFAULT_WINDOW_SIZE_Y = 600;
 const MIN_WINDOW_SIZE_X = Math.round(1920 / 3);
 const MIN_WINDOW_SIZE_Y = Math.round(1080 / 3);
 
@@ -14,18 +18,35 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-//TODO: Replace with proper Typings
-let options:Options = {
-  sizeX: undefined,
-  sizeY: undefined,
-  posX: undefined,
-  posY: undefined
-};
+//load settings
+const files = fs.readdirSync('./');
+let foundOptionsFile = false;
+for(const file of files){
+  if(file === 'options.json'){
+    foundOptionsFile = true;
+    break;
+  }
+}
+
+var options:Options;
+
+if(foundOptionsFile){
+  options = JSON.parse(fs.readFileSync('./options.json').toString());
+}else{
+  options = {
+    build: -1,
+    sizeX: undefined,
+    sizeY: undefined,
+    posX: undefined,
+    posY: undefined,
+    hardwareAcceleration: true
+  };
+}
 
 async function createWindow() {
   //load window size, if no set in option use defaults
-  var sizeX = options.sizeX || 1050;
-  var sizeY = options.sizeY || 600;
+  var sizeX = options.sizeX || DEFAULT_WINDOW_SIZE_X;
+  var sizeY = options.sizeY || DEFAULT_WINDOW_SIZE_Y;
 
   //make sure that the values are not to small
   sizeX < MIN_WINDOW_SIZE_X ? MIN_WINDOW_SIZE_X : sizeX;
@@ -45,6 +66,12 @@ async function createWindow() {
     posY = options.posY as number;
   }
 
+  //write options into options object
+  options.sizeX = sizeX;
+  options.sizeY = sizeY;
+  options.posX = posX;
+  options.posY = posY;
+
   // Create the browser window.
   const win = new BrowserWindow({
     show: false,
@@ -56,7 +83,7 @@ async function createWindow() {
     minWidth: MIN_WINDOW_SIZE_X,
     minHeight: MIN_WINDOW_SIZE_Y,
     autoHideMenuBar: true,
-    frame: false,
+    frame: true,
     acceptFirstMouse: true,
     roundedCorners: false,
     title: 'Launcher',
@@ -70,7 +97,22 @@ async function createWindow() {
     }
   })
 
+  //show the window when its fully rendered
   win.once('ready-to-show', () => {win.show()})
+
+  //save window settings on closing
+  win.on('close', () => {
+    const {width, height} = win.getBounds();
+    const pos = win.getPosition();
+
+    options.sizeX = width;
+    options.sizeY = height;
+    options.posX = pos[0];
+    options.posY = pos[1];
+
+    const fileContent = JSON.stringify(options);
+    fs.writeFileSync('./options.json', fileContent);
+  })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
@@ -82,6 +124,13 @@ async function createWindow() {
     win.loadURL('app://./index.html')
   }
 }
+
+//disable Hardware acceleration if its defined in the options
+if(!options.hardwareAcceleration){
+  app.disableHardwareAcceleration()
+  console.log('disabled Hardware Acceleration')
+}
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -105,6 +154,7 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
+      console.log('[DEV] Trying to Install VUE3 Detools Extension')
       await installExtension(VUEJS3_DEVTOOLS)
     } catch (e: any) {
       console.error('Vue Devtools failed to install:', e.toString())
--- src/types/options.ts
+++ src/types/options.ts
@@ -1,6 +1,11 @@
 export default interface Options {
+    //current version of the game
+    build: number,
+    //window position and size
     sizeX: number | undefined,
     sizeY: number | undefined,
     posX: number | undefined,
     posY: number | undefined,
+    //specific options that are user/system prefrence
+    hardwareAcceleration: boolean
 }
