089ac4b299437968ff4f03fca648f7d015c05842
Snoweuph <domsaeume@gmail.com>
2022-03-29 21:43:12 +0200

    basic structuring of vue part of project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import HelloWorld from "./components/HelloWorld.vue";
+  
+  export default defineComponent({
+    name: "App",
+    components: {
+      HelloWorld,
+    },
+  });
+</script>
+
 <template>
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/scss/themes/default-theme.scss';
+@import './assets/scss/base.scss';
 </style>
