f7e1192eea058bd65398ee4074fd7472de69b3ec
Snoweuph <domsaeume@gmail.com>
2022-03-29 21:53:13 +0200

    fixed linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
   
-  export default defineComponent({
-    name: 'HelloWorld',
-    props: {
-      msg: String,
-    },
-  });
+export default defineComponent({
+	name: 'HelloWorld',
+	props: {
+		msg: String,
+	},
+});
 </script>
 
 <template>
