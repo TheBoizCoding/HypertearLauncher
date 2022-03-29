dc9df87fc9cf76a2e3208537853d0f61dde89778
Snoweuph <domsaeume@gmail.com>
2022-03-29 20:28:46 +0200

    setup basics for electron

--- src/background.ts
+++ src/background.ts
@@ -1,20 +1,65 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, screen} from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+import Options from './types/options'
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const MIN_WINDOW_SIZE_X = Math.round(1920 / 3);
+const MIN_WINDOW_SIZE_Y = Math.round(1080 / 3);
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
+//TODO: Replace with proper Typings
+let options:Options = {
+  sizeX: undefined,
+  sizeY: undefined,
+  posX: undefined,
+  posY: undefined
+};
+
 async function createWindow() {
+  //load window size, if no set in option use defaults
+  var sizeX = options.sizeX || 1050;
+  var sizeY = options.sizeY || 600;
+
+  //make sure that the values are not to small
+  sizeX < MIN_WINDOW_SIZE_X ? MIN_WINDOW_SIZE_X : sizeX;
+  sizeY < MIN_WINDOW_SIZE_Y ? MIN_WINDOW_SIZE_Y : sizeY;
+
+  //get the screen position for the window and the screen to use
+  var posX:number;
+  var posY:number;
+
+  if(options.posX == NaN || options.posY == NaN) {
+    const primaryDisplay = screen.getPrimaryDisplay();
+    const {width, height} = primaryDisplay.workAreaSize;
+    posX = (width / 2) - (sizeX / 2);
+    posY = (height / 2) - (sizeY / 2);
+  } else {
+    posX = options.posX as number;
+    posY = options.posY as number;
+  }
+
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    show: false,
+    paintWhenInitiallyHidden: true,
+    width: sizeX,
+    height: sizeY,
+    x: posX,
+    y: posY,
+    minWidth: MIN_WINDOW_SIZE_X,
+    minHeight: MIN_WINDOW_SIZE_Y,
+    autoHideMenuBar: true,
+    frame: false,
+    acceptFirstMouse: true,
+    roundedCorners: false,
+    title: 'Launcher',
     webPreferences: {
 
       // Use pluginOptions.nodeIntegration, leave this alone
@@ -25,10 +70,12 @@ async function createWindow() {
     }
   })
 
+  win.once('ready-to-show', () => {win.show()})
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (process.env.SHOW_DEVTOOLS) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
---
+++ src/types/options.ts
@@ -0,0 +1,6 @@
+export default interface Options {
+    sizeX: number | undefined,
+    sizeY: number | undefined,
+    posX: number | undefined,
+    posY: number | undefined,
+}
